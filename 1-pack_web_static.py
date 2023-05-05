44d2344426b162881796900b45717450962023b9
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-05-05 16:42:50 +0300

    Compressing folder using fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from
+the contents of the web_static folder."""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """Creates a compressed archive of the web_static folder."""
+    try:
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
+
+        now = datetime.now()
+        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+                now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+        command = f"tar -cvzf versions/{archive_name} web_static"
+        local(command)
+
+        return f"versions/{archive_name}"
+
+    except:
+        return None
+
