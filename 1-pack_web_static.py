6bebe8cea4714ce614ac2da6f109f9dd4a2468c8
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-05-06 16:57:19 +0300

    Checking error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Function that compresses the web_static folder. """
+
+
 from fabric.api import local
 from datetime import datetime
 import os
 
 
 def do_pack():
+    '''
+        Function that creates a compressed archive of
+        the web_static folder.
+    '''
     try:
         if not os.path.exists("versions"):
             local('mkdir versions')
 
         now = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_name = "web_static_{}.tgz".format(now.strftime(f))
+        archive_name = "web_static_{}.tgz".format(
+                now.year, now.month, now.day, now.hour,
+                now.minute, now.second)
 
         command = f"tar -cvzf versions/{archive_name} web_static"
         local(command)
