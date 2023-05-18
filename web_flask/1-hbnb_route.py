03e56d03b2b2f7365dc5c7e2867f5440f649ee4e
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-05-18 13:07:50 +0300

    hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Script that starts a Flask Web app."""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
