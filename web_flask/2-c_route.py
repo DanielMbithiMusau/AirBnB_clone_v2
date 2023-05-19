b230141c3b396f2b7842660848f98a0e55848dcd
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-05-19 12:44:55 +0300

    Checking

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,7 +17,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     text = escape(text).replace('_', ' ')
-    return f"C {text}"
+    return "C {}".format(text)
 
 
 if __name__ == '__main__':
